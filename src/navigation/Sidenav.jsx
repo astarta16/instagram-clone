d7abb6b5166d156752cb4942114bd8ecb49d686a
astarta16 <ngedeshuri2001@gmail.com>
2023-12-22 12:30:36 +0400

    add icons and sidenav styles

--- src/navigation/Sidenav.jsx
+++ src/navigation/Sidenav.jsx
@@ -1,12 +1,60 @@
-import React from 'react';
+import React from "react";
 import "./Sidenav.css";
+import HomeIcon from '@mui/icons-material/Home';
+import SearchIcon from "@mui/icons-material/Search";
+import ExploreIcon from "@mui/icons-material/Explore";
+import SlideshowIcon from "@mui/icons-material/Slideshow";
+import ChatIcon from "@mui/icons-material/Chat";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import MenuIcon from "@mui/icons-material/Menu";
 
 function Sidenav() {
   return (
-    <div>
-      sidenav
+    <div className="sidenav">
+      <img
+        className="sidenav_logo"
+        src="https://www.pngkey.com/png/full/828-8286178_mackeys-work-needs-no-elaborate-presentation-or-distracting.png"
+        alt=""
+      />
+      <div className="sidenav_buttons">
+        <button className="sidenav_button">
+          <HomeIcon />
+          <span>Home</span>
+        </button>
+        <button className="sidenav_button">
+          <SearchIcon />
+          <span>Search</span>
+        </button>
+        <button className="sidenav_button">
+          <ExploreIcon />
+          <span>Explore</span>
+        </button>
+        <button className="sidenav_button">
+          <SlideshowIcon />
+          <span>Reels</span>
+        </button>
+        <button className="sidenav_button">
+          <ChatIcon />
+          <span>Messages</span>
+        </button>
+        <button className="sidenav_button">
+          < FavoriteBorderIcon/>
+          <span>Notification</span>
+        </button>
+        <button className="sidenav_button">
+          <AddCircleOutlineIcon />
+          <span>Create</span>
+        </button>
+      </div>
+      <div className="sidenav_more">
+        <button className="sidenav_button">
+          < MenuIcon/>
+          <span>More</span>
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Sidenav
+export default Sidenav;
