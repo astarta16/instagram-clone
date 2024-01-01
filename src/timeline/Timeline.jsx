3add724e86ded5cc9a76d0c51f88c3839c5f5a51
astarta16 <ngedeshuri2001@gmail.com>
2024-01-01 10:15:27 +0400

    add posts component

--- src/timeline/Timeline.jsx
+++ src/timeline/Timeline.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import "./Timeline.css";
 import Suggestion from "./Sugesstion";
+import Post from "./posts/Post";
 
 function Timeline() {
   return (
     <div className="timeline">
-      <div className="timeline_left">timeline</div>
+      <div className="timeline_left">
+        <div className="timeline_posts">
+          <Post/>
+        </div>
+      </div>
       <div className="timeline_right">
         <Suggestion />
       </div>
