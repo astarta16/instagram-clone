dc00fd249953f6b9412dafbba7a5143be11a1b27
astarta16 <ngedeshuri2001@gmail.com>
2024-01-02 12:33:22 +0400

    add styles in the post section

--- src/timeline/posts/Post.jsx
+++ src/timeline/posts/Post.jsx
@@ -18,18 +18,18 @@ function Post() {
         <img src="https://images.unsplash.com/photo-1575936123452-b67c3203c357?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
       </div>
       <div className="post_footer">
-        <div className="post_footerIcons">
-          <div className="post_iconsMain">
-            <FavoriteBorder className="postIcon"/>
-            <ChatBubbleOutline className="postIcon"/>
-            <Telegram className="postIcon"/>
-          </div>
-          <div className="post_iconSave">
-            <BookmarkBorder className="postIcon" />
-          </div>
-        </div>
-        liked by 21
-      </div>
+  <div className="post_footerIcons"> 
+    <div className="post_iconsMain">
+      <FavoriteBorder className="postIcon"/>
+      <ChatBubbleOutline className="postIcon"/>
+      <Telegram className="postIcon"/>
+    </div>
+    <div className="post_iconSave">
+      <BookmarkBorder className="postIcon" />
+    </div>
+  </div>
+  liked by 21
+</div>
     </div>
   );
 }
