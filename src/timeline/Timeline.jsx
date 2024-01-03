901942cbe432c156e94414e3e4c834bfeef09fa3
astarta16 <ngedeshuri2001@gmail.com>
2024-01-03 11:51:02 +0400

    change data lists add images

--- src/timeline/Timeline.jsx
+++ src/timeline/Timeline.jsx
@@ -7,31 +7,34 @@ function Timeline() {
   const [posts, setPosts] = useState([
     {
       user: "nini",
-      postImage: "",
-      likes:12,
-      timespamp: "2d"
+      postImage:
+        "https://images.unsplash.com/photo-1575936123452-b67c3203c357?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      likes: 15,
+      timespamp: "1d",
     },
     {
-      user: "nini",
-      postImage: "",
-      likes:12,
-      timespamp: "2d"
+      user: "tata",
+      postImage:
+        "https://images.unsplash.com/photo-1575936123452-b67c3203c357?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      likes: 100,
+      timespamp: "4d",
     },
     {
-      user: "nini",
-      postImage: "",
-      likes:12,
-      timespamp: "2d"
+      user: "sandro",
+      postImage:
+        "https://images.unsplash.com/photo-1575936123452-b67c3203c357?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      likes: 43,
+      timespamp: "2d",
     },
-  ])
+  ]);
 
   return (
     <div className="timeline">
       <div className="timeline_left">
         <div className="timeline_posts">
-          <Post/>
-          <Post/>
-          <Post/>
+          <Post />
+          <Post />
+          <Post />
         </div>
       </div>
       <div className="timeline_right">
