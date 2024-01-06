ea7d83ad4745e810957ebace3473c584e9042313
astarta16 <ngedeshuri2001@gmail.com>
2024-01-07 01:47:57 +0400

    add follow button

--- src/timeline/Sugesstion.jsx
+++ src/timeline/Sugesstion.jsx
@@ -6,7 +6,10 @@ function Suggestion() {
     <div className='suggestions'>
       <div className="suggestions_title">sugestions for you</div>
       <div className="sugesstions_username">
-        <div className='suggestion_username'></div>
+        <div className='suggestion_username'>
+          <div className='username_left'></div>
+          <button className='follow_button'>Follow</button>
+        </div>
       </div>
       
     </div>
