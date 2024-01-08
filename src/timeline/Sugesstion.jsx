f02d29bbddd30df013d3c7644f5296f0cb366115
astarta16 <ngedeshuri2001@gmail.com>
2024-01-08 15:54:51 +0400

    start suggestions section

--- src/timeline/Sugesstion.jsx
+++ src/timeline/Sugesstion.jsx
@@ -1,19 +1,65 @@
-import React from 'react';
-import './Sugesstion.css';
+import React from "react";
+import "./Sugesstion.css";
+import { Avatar } from "@mui/material";
 
 function Suggestion() {
   return (
-    <div className='suggestions'>
+    <div className="suggestions">
       <div className="suggestions_title">sugestions for you</div>
       <div className="sugesstions_username">
-        <div className='suggestion_username'>
-          <div className='username_left'></div>
-          <button className='follow_button'>Follow</button>
+
+        <div className="suggestion_username">
+          <div className="username_left">
+            <span className="avatar">
+              <Avatar>R</Avatar>
+            </span>
+            <div className="username_info">
+              <span className="username">Nini</span>
+              <span className="relation">New to instagram</span>
+            </div>
+          </div>
+          <button className="follow_button">Follow</button>
         </div>
+        <div className="suggestion_username">
+          <div className="username_left">
+            <span className="avatar">
+              <Avatar>R</Avatar>
+            </span>
+            <div className="username_info">
+              <span className="username">Nini</span>
+              <span className="relation">New to instagram</span>
+            </div>
+          </div>
+          <button className="follow_button">Follow</button>
+        </div>
+        <div className="suggestion_username">
+          <div className="username_left">
+            <span className="avatar">
+              <Avatar>R</Avatar>
+            </span>
+            <div className="username_info">
+              <span className="username">Nini</span>
+              <span className="relation">New to instagram</span>
+            </div>
+          </div>
+          <button className="follow_button">Follow</button>
+        </div>
+        <div className="suggestion_username">
+          <div className="username_left">
+            <span className="avatar">
+              <Avatar>R</Avatar>
+            </span>
+            <div className="username_info">
+              <span className="username">Nini</span>
+              <span className="relation">New to instagram</span>
+            </div>
+          </div>
+          <button className="follow_button">Follow</button>
+        </div>
+
       </div>
-      
     </div>
-  )
+  );
 }
 
-export default Suggestion
+export default Suggestion;
