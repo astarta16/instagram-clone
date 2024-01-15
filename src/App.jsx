a87a924983106a174b89c4b85b80fd6fd67694d8
astarta16 <ngedeshuri2001@gmail.com>
2024-01-15 18:09:50 +0400

    add authpage router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
+import AuthPage from "./pages/Authpage/AuthPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />}></Route>
+      <Route path="/" element={<AuthPage />}></Route>
+
     </Routes>
   );
 }
