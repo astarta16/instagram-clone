8911f9fae594a695f5a88a4754d4a07046e2467b
astarta16 <ngedeshuri2001@gmail.com>
2024-01-15 18:35:01 +0400

    add image in the auth page

--- src/pages/Authpage/AuthPage.jsx
+++ src/pages/Authpage/AuthPage.jsx
@@ -1,14 +1,19 @@
-import { Container, Flex } from '@chakra-ui/react'
-import React from 'react'
+import { Container, Flex, Box, Image, VStack } from "@chakra-ui/react";
+import React from "react";
 
-const AuthPage =() => {
+const AuthPage = () => {
   return (
-   <Flex minH={"100vh"} justifyContent={"center"} alignItems={"center"} px = {4}>
-    <Container maxW={Container.md} padding={0}>
+    <Flex minH={"100vh"} justifyContent={"center"} alignItems={"center"} px={4}>
+      <Container maxW={Container.md} padding={0}>
+        <Box display={{ base: "none", md: "block" }}>
+          <Image src="/auth.png" h={650} alt="Phone img"></Image>
+        </Box>
+        <VStack>
+            
+        </VStack>
+      </Container>
+    </Flex>
+  );
+};
 
-    </Container>
-   </Flex>
-  )
-}
-
-export default AuthPage
+export default AuthPage;
