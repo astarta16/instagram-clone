49a641864be251329bcba276b95f0b69f49824cb
astarta16 <ngedeshuri2001@gmail.com>
2024-01-17 01:33:34 +0400

    add instagram logo and start auth form

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -1,5 +1,14 @@
+import { Box, VStack,  Image } from "@chakra-ui/react";
+
 const AuthForm = () => {
-  return <div>authform</div>;
+  return <>
+  <Box border={"1px solid gray"} borderRadius={4} padding={5}>
+    <VStack spacing={4}>
+      <Image  src= '/logo.png' h={24} cursor ={"pointer"} alt = "instagram logo"></Image>
+
+    </VStack>
+  </Box>
+  </>
 };
 
 export default AuthForm;
--- src/pages/Authpage/AuthPage.jsx
+++ src/pages/Authpage/AuthPage.jsx
@@ -6,18 +6,19 @@ const AuthPage = () => {
   return (
     <Flex minH={"100vh"} justifyContent={"center"} alignItems={"center"} px={4}>
       <Container maxW={Container.md} padding={0}>
-        <Box display={{ base: "none", md: "block" }}>
-          <Image src="/auth.png" h={650} alt="Phone img"></Image>
-        </Box>
-        <VStack spacing={4} align={"stretch"}>
-          <AuthForm />
+        <Flex justifyContent={"center"} alignItems={"center"} gap={10}>
+          <Box display={{ base: "none", md: "block" }}>
+            <Image src="/auth.png" h={650} alt="Phone img"></Image>
+          </Box>
+          <VStack spacing={4} align={"stretch"}>
+            <AuthForm />
             <Box textAlign={"center"}>Get the app.</Box>
             <Flex gap={5} justifyContent={"center"}>
-              <Image src="/playstore.png" h={"10"} alt="playstore logo"/>
-              <Image src="/microsoft.png" h={"10"} alt="microsoft logo"/>
-
+              <Image src="/playstore.png" h={"10"} alt="playstore logo" />
+              <Image src="/microsoft.png" h={"10"} alt="microsoft logo" />
             </Flex>
-        </VStack>
+          </VStack>
+        </Flex>
       </Container>
     </Flex>
   );
