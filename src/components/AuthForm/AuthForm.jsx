2b39058621704aa56abc11893c5d07add4ba2701
astarta16 <ngedeshuri2001@gmail.com>
2024-01-22 11:20:42 +0400

    add inputs in the auth form

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -1,14 +1,21 @@
-import { Box, VStack,  Image } from "@chakra-ui/react";
+import { Box, VStack, Image, Input } from "@chakra-ui/react";
 
 const AuthForm = () => {
-  return <>
-  <Box border={"1px solid gray"} borderRadius={4} padding={5}>
-    <VStack spacing={4}>
-      <Image  src= '/logo.png' h={24} cursor ={"pointer"} alt = "instagram logo"></Image>
-
-    </VStack>
-  </Box>
-  </>
+  return (
+    <>
+      <Box border={"1px solid gray"} borderRadius={4} padding={5}>
+        <VStack spacing={4}>
+          <Image
+            src="/logo.png"
+            h={24}
+            cursor={"pointer"}
+            alt="instagram logo"></Image>
+          <Input placeholder="Email" fontSize={14} type="email"></Input>
+          <Input placeholder="Password" fontSize={14} type="password"></Input>
+        </VStack>
+      </Box>
+    </>
+  );
 };
 
 export default AuthForm;
