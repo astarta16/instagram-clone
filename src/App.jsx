1ec4751f19b5f0dc8d95d8461a2ec72960e43bed
astarta16 <ngedeshuri2001@gmail.com>
2024-01-24 13:34:04 +0400

    wrap app with pagelayout

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 import AuthPage from "./pages/Authpage/AuthPage";
+import PageLayout from "./Layouts/PageLayout/PageLayout";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />}></Route>
-      <Route path="/auth" element={<AuthPage />}></Route>
-
-    </Routes>
+    <PageLayout>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/auth" element={<AuthPage />}></Route>
+      </Routes>
+    </PageLayout>
   );
 }
 
