b6263f5bf9ad4d9b425406ace4294dd2faca9644
astarta16 <ngedeshuri2001@gmail.com>
2024-01-31 11:45:47 +0400

    add states and logos

--- src/components/FeedPosts/PostFooter.jsx
+++ src/components/FeedPosts/PostFooter.jsx
@@ -1,11 +1,15 @@
 import { Flex, Box } from '@chakra-ui/react'
-import React from 'react'
+import React, { useState } from 'react'
+import {NotificationLogo,UnlikeLogo } from '../../assets/constants'
 
 function PostFooter() {
+const[liked, setLiked] = useState(false);
+const [likes, setLikes] = useState(1000)
+
   return <>
   <Flex alignItems={"center"} gap={4} w={"full"} pt={0} mb={2} mt={"auto"}>
     <Box>
-
+{!liked ? (<NotificationLogo />) : (<UnlikeLogo />)}
     </Box>
   </Flex>
   
