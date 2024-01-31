81d9f52e635635e97ddcf5ca8f42cd7e25f30680
astarta16 <ngedeshuri2001@gmail.com>
2024-01-31 12:09:56 +0400

    add likes button

--- src/components/FeedPosts/PostFooter.jsx
+++ src/components/FeedPosts/PostFooter.jsx
@@ -1,19 +1,29 @@
-import { Flex, Box } from '@chakra-ui/react'
-import React, { useState } from 'react'
-import {NotificationLogo,UnlikeLogo } from '../../assets/constants'
+import { Flex, Box } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { NotificationLogo, UnlikeLogo } from "../../assets/constants";
 
-function PostFooter() {
-const[liked, setLiked] = useState(false);
-const [likes, setLikes] = useState(1000)
+const PostFooter = () => {
+  const [liked, setLiked] = useState(false);
+  const [likes, setLikes] = useState(1000);
 
-  return <>
-  <Flex alignItems={"center"} gap={4} w={"full"} pt={0} mb={2} mt={"auto"}>
-    <Box>
-{!liked ? (<NotificationLogo />) : (<UnlikeLogo />)}
-    </Box>
-  </Flex>
-  
-  </>
-}
+  const handleLike = () => {
+    if (liked) {
+      setLiked(false);
+      setLikes(liked - 1);
+    } else {
+      setLiked(true);
+      setLikes(likes + 1);
+    }
+  };
+  return (
+    <>
+      <Flex alignItems={"center"} gap={4} w={"full"} pt={0} mb={2} mt={"auto"}>
+        <Box onClick={handleLike} cursor={"pointer"}>
+          {!liked ? <NotificationLogo /> : <UnlikeLogo />}
+        </Box>
+      </Flex>
+    </>
+  );
+};
 
-export default PostFooter
+export default PostFooter;
