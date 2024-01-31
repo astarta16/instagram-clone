c9354598c6473114af196874725a63bdb8f63839
astarta16 <ngedeshuri2001@gmail.com>
2024-01-31 11:39:19 +0400

    start postheader section

--- src/components/FeedPosts/PostFooter.jsx
+++ src/components/FeedPosts/PostFooter.jsx
@@ -1,11 +1,15 @@
+import { Flex } from '@chakra-ui/react'
 import React from 'react'
 
 function PostFooter() {
-  return (
-    <div>
-      postfooter
-    </div>
-  )
+  return <>
+  <Flex alignItems={"center"} gap={4} w={"full"} pt={0} mb={2} mt={"auto"}>
+    <Box>
+      
+    </Box>
+  </Flex>
+  
+  </>
 }
 
 export default PostFooter
