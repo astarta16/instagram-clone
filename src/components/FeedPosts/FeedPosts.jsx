7f4d45b19d475a15c60f3414977eba4790df6655
astarta16 <ngedeshuri2001@gmail.com>
2024-02-06 01:07:13 +0400

    add skelaton

--- src/components/FeedPosts/FeedPosts.jsx
+++ src/components/FeedPosts/FeedPosts.jsx
@@ -1,33 +1,42 @@
-import { Container, Flex, Skeleton, SkeletonCircle, VStack, useAccordion } from '@chakra-ui/react'
-import React, { useEffect, useState } from 'react'
-import FeedPost from './FeedPost'
+import {
+  Container,
+  Flex,
+  Skeleton,
+  SkeletonCircle,
+  VStack,
+  useAccordion,
+} from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import FeedPost from "./FeedPost";
 
-const FeedPosts =() => {
+const FeedPosts = () => {
   const [isLoading, setIsloading] = useState(true);
 
   useEffect(() => {
     setTimeout(() => {
-      setIsloading(false)
-    }, 2000)
-  }, [])
+      setIsloading(false);
+    }, 2000);
+  }, []);
   return (
-   <Container maxW={"container.sm"} py={10} px={2}>
-    {isLoading && [0,1,2,3].map((_,idx) => (
-      <VStack key={idx} gap={4} alignItems={"flex-start"} mb={10}>
-        <Flex gap="2">
-          <SkeletonCircle size='10'/>
-          <VStack></VStack>
-          
-        </Flex>
+    <Container maxW={"container.sm"} py={10} px={2}>
+      {isLoading &&
+        [0, 1, 2, 3].map((_, idx) => (
+          <VStack key={idx} gap={4} alignItems={"flex-start"} mb={10}>
+            <Flex gap="2">
+              <SkeletonCircle size="10" />
+              <VStack gap={2} alignItems={"flex-start"}>
+                <SkeletonCircle height="10px" w={"200px"} />
+                <SkeletonCircle height="10px" w={"200px"} />
+              </VStack>
+            </Flex>
+          </VStack>
+        ))}
+      <FeedPost img="/img1.png" username="ssd" avatar="/img1.png" />
+      <FeedPost img="/img2.png" username="ssd" avatar="/img1.png" />
+      <FeedPost img="/img3.png" username="ssd" avatar="/img1.png" />
+      <FeedPost img="/img4.png" username="ssd" avatar="/img1.png" />
+    </Container>
+  );
+};
 
-      </VStack>
-    ))}
-    <FeedPost img="/img1.png" username="ssd" avatar = "/img1.png"/>
-    <FeedPost img="/img2.png" username="ssd" avatar = "/img1.png"/>
-    <FeedPost img="/img3.png" username="ssd" avatar = "/img1.png"/>
-    <FeedPost img="/img4.png" username="ssd" avatar = "/img1.png"/>
-   </Container>
-  )
-}
-
-export default FeedPosts
+export default FeedPosts;
