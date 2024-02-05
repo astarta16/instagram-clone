b4a712c20d57a99fa85f8665c716e2905a76f0f1
astarta16 <ngedeshuri2001@gmail.com>
2024-02-06 01:02:52 +0400

    add loading states

--- src/components/FeedPosts/FeedPosts.jsx
+++ src/components/FeedPosts/FeedPosts.jsx
@@ -1,10 +1,27 @@
-import { Container } from '@chakra-ui/react'
-import React from 'react'
+import { Container, Flex, Skeleton, SkeletonCircle, VStack, useAccordion } from '@chakra-ui/react'
+import React, { useEffect, useState } from 'react'
 import FeedPost from './FeedPost'
 
 const FeedPosts =() => {
+  const [isLoading, setIsloading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsloading(false)
+    }, 2000)
+  }, [])
   return (
    <Container maxW={"container.sm"} py={10} px={2}>
+    {isLoading && [0,1,2,3].map((_,idx) => (
+      <VStack key={idx} gap={4} alignItems={"flex-start"} mb={10}>
+        <Flex gap="2">
+          <SkeletonCircle size='10'/>
+          <VStack></VStack>
+          
+        </Flex>
+
+      </VStack>
+    ))}
     <FeedPost img="/img1.png" username="ssd" avatar = "/img1.png"/>
     <FeedPost img="/img2.png" username="ssd" avatar = "/img1.png"/>
     <FeedPost img="/img3.png" username="ssd" avatar = "/img1.png"/>
