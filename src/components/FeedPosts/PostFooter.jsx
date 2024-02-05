cf07e7291bbd4ea47d67233a56f052643c9d8dda
astarta16 <ngedeshuri2001@gmail.com>
2024-02-06 00:54:29 +0400

    add props in the post footer

--- src/components/FeedPosts/PostFooter.jsx
+++ src/components/FeedPosts/PostFooter.jsx
@@ -6,7 +6,7 @@ import {
   UnlikeLogo,
 } from "../../assets/constants";
 
-const PostFooter = () => {
+const PostFooter = ({username}) => {
   const [liked, setLiked] = useState(false);
   const [likes, setLikes] = useState(1000);
 
@@ -34,7 +34,7 @@ const PostFooter = () => {
         {likes} likes
       </Text>
       <Text fontWeight={700} fontSize={"sm"}>
-        asaprogrammer_{""}
+       {username} {""}
         <Text as="span" fontWeight={400}>
           Feeling good
         </Text>
