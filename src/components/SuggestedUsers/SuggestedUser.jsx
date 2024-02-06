7b2021eaf18bd8cb30782d61ba1f5ae54d75105a
astarta16 <ngedeshuri2001@gmail.com>
2024-02-07 00:06:37 +0400

    start suggestions section

---
+++ src/components/SuggestedUsers/SuggestedUser.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SuggestedUser() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SuggestedUser
---
+++ src/components/SuggestedUsers/SuggestedUsers.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SuggestedUsers() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SuggestedUsers
