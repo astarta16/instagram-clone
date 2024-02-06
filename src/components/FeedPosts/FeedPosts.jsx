bdf7595087c573382bb9aeebedc22b00ee1ef630
astarta16 <ngedeshuri2001@gmail.com>
2024-02-07 00:01:54 +0400

    delete one skeleton

--- src/components/FeedPosts/FeedPosts.jsx
+++ src/components/FeedPosts/FeedPosts.jsx
@@ -27,7 +27,6 @@ const FeedPosts = () => {
               <SkeletonCircle size="10" />
               <VStack gap={2} alignItems={"flex-start"}>
                 <SkeletonCircle height="10px" w={"200px"} />
-                <SkeletonCircle height="10px" w={"200px"} />
               </VStack>
             </Flex>
             <Skeleton w={"full"}>
