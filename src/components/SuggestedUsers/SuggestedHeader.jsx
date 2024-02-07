8c1c96f93daad94aa6d0217d677cc5039e1465f3
astarta16 <ngedeshuri2001@gmail.com>
2024-02-08 00:28:49 +0400

    correct styles

--- src/components/SuggestedUsers/SuggestedHeader.jsx
+++ src/components/SuggestedUsers/SuggestedHeader.jsx
@@ -4,7 +4,7 @@ import React from "react";
 function SuggestedHeader() {
   return (
     <Flex justifyContent={"space-between"} align={"center"} w={"full"}>
-      <Flex>
+      <Flex alignItems={"center"} gap={2}>
         <Avatar name="As a programmer" size={"lg"} src="/profilepic.png" />
         <Box fontSize={12} fontWeight={"bold"}>
           asaprogrammer_
