080e737b23f361417bfc239bce0ffc02ffd660b9
astarta16 <ngedeshuri2001@gmail.com>
2024-02-15 11:47:58 +0400

    add text see all

--- src/components/SuggestedUsers/SuggestedUsers.jsx
+++ src/components/SuggestedUsers/SuggestedUsers.jsx
@@ -1,19 +1,24 @@
-import { Text, Flex, VStack } from '@chakra-ui/react'
-import React from 'react'
-import SuggestedHeader from './SuggestedHeader'
+import { Text, Flex, VStack } from "@chakra-ui/react";
+import React from "react";
+import SuggestedHeader from "./SuggestedHeader";
 
 function SuggestedUsers() {
-  return <VStack py={8} px={6} gap={4}>
-    <SuggestedHeader />
+  return (
+    <VStack py={8} px={6} gap={4}>
+      <SuggestedHeader />
 
-<Flex alignItems={"center"} justifyContent={"space-between"} w={"full"}>
-  <Text fontSize={12} fontWeight={"bold"} color={"gray.500"}>
-    Suggested for you
-
-  </Text>
-
-</Flex>
-  </VStack>
+      <Flex alignItems={"center"} justifyContent={"space-between"} w={"full"}>
+        <Text fontSize={12} fontWeight={"bold"} color={"gray.500"}>
+          Suggested for you
+        </Text>
+        <Text
+          fontSize={12}
+          fontWeight={"bold"}
+          _hover={{ color: "gray.400" }}
+          cursor={"pointer"}>See All</Text>
+      </Flex>
+    </VStack>
+  );
 }
 
-export default SuggestedUsers
+export default SuggestedUsers;
