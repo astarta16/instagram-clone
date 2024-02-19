46dbb04b9d5f05990deb7964c994a5bbde8c8666
astarta16 <ngedeshuri2001@gmail.com>
2024-02-19 13:53:47 +0400

    working for suggesteduser buttons

--- src/components/SuggestedUsers/SuggestedUser.jsx
+++ src/components/SuggestedUsers/SuggestedUser.jsx
@@ -1,11 +1,16 @@
+import { Avatar, Button, Flex } from '@chakra-ui/react'
 import React from 'react'
 
-function SuggestedUser({followers, name, avatar}) {
-  return (
-    <div>
-      
-    </div>
-  )
+const  SuggestedUser = ({followers, name, avatar}) => {
+  return <Flex w={"full"}
+  justifyContent={"space-between"} alignItems={"center"}>
+    <Flex>
+      <Avatar src={avatar} name={name}  size={"md"}/>
+    </Flex>
+    <Button></Button>
+  </Flex>
+   
+  
 }
 
 export default SuggestedUser
