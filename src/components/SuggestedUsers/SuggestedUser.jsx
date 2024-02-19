375904277e7a6319a5108af5aa062154403b2842
astarta16 <ngedeshuri2001@gmail.com>
2024-02-19 13:55:06 +0400

    format file

--- src/components/SuggestedUsers/SuggestedUser.jsx
+++ src/components/SuggestedUsers/SuggestedUser.jsx
@@ -1,16 +1,15 @@
-import { Avatar, Button, Flex } from '@chakra-ui/react'
-import React from 'react'
+import { Avatar, Button, Flex } from "@chakra-ui/react";
+import React from "react";
 
-const  SuggestedUser = ({followers, name, avatar}) => {
-  return <Flex w={"full"}
-  justifyContent={"space-between"} alignItems={"center"}>
-    <Flex>
-      <Avatar src={avatar} name={name}  size={"md"}/>
+const SuggestedUser = ({ followers, name, avatar }) => {
+  return (
+    <Flex w={"full"} justifyContent={"space-between"} alignItems={"center"}>
+      <Flex alignItems={"center"} gap={2}>
+        <Avatar src={avatar} name={name} size={"md"} />
+      </Flex>
+      <Button></Button>
     </Flex>
-    <Button></Button>
-  </Flex>
-   
-  
-}
+  );
+};
 
-export default SuggestedUser
+export default SuggestedUser;
