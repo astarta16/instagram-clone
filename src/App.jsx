6287fc808af238d3de65118f4486781e7cc9bbc0
astarta16 <ngedeshuri2001@gmail.com>
2024-02-21 13:19:30 +0400

    add router

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 import AuthPage from "./pages/Authpage/AuthPage";
 import PageLayout from "./Layouts/PageLayout/PageLayout";
+import ProfilePage from "./pages/Profilepage/ProfilePage";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
         <Route path="/auth" element={<AuthPage />}></Route>
+        <Route path="/:username" element={<ProfilePage />}></Route>
       </Routes>
     </PageLayout>
   );
