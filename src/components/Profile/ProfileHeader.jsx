ad582a9b3d39893dba481f60d8592f08c4c07fd0
astarta16 <ngedeshuri2001@gmail.com>
2024-02-26 19:10:45 +0400

    add flex and avatargroup

--- src/components/Profile/ProfileHeader.jsx
+++ src/components/Profile/ProfileHeader.jsx
@@ -1,11 +1,12 @@
+import { AvatarGroup, Flex } from '@chakra-ui/react'
 import React from 'react'
 
 function ProfileHeader() {
-  return (
-    <div>
-      header
-    </div>
-  )
+  return <Flex gap={{base:4, sm:10}}>
+    <AvatarGroup></AvatarGroup>
+
+  </Flex>
+   
 }
 
 export default ProfileHeader
