396deb2742e4a10253d24591d34c9119ad5ef0eb
astarta16 <ngedeshuri2001@gmail.com>
2024-03-01 16:42:17 +0400

    add styles for avatar

--- src/components/Profile/ProfileHeader.jsx
+++ src/components/Profile/ProfileHeader.jsx
@@ -1,12 +1,20 @@
-import { AvatarGroup, Flex } from '@chakra-ui/react'
-import React from 'react'
+import { Avatar, AvatarGroup, Flex } from "@chakra-ui/react";
+import React from "react";
 
 function ProfileHeader() {
-  return <Flex gap={{base:4, sm:10}}>
-    <AvatarGroup size={{base:"xl", md:"2xl"}} justifyContent={"center"} alignSelf={"flex-start"} mx={"auto"}></AvatarGroup>
+  return (
+    <Flex gap={{ base: 4, sm: 10 }}>
+      <AvatarGroup
+        size={{ base: "xl", md: "2xl" }}
+        justifyContent={"center"}
+        alignSelf={"flex-start"}
+        mx={"auto"}>
+        <Avatar name="As a Programmer" src="/profilepic.png"></Avatar>
+      </AvatarGroup>
 
-  </Flex>
-   
+      
+    </Flex>
+  );
 }
 
-export default ProfileHeader
+export default ProfileHeader;
