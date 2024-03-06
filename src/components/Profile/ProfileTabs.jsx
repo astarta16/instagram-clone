74d977842db6e5e1a27ead738b5c2ea0fff41f98
astarta16 <ngedeshuri2001@gmail.com>
2024-03-06 14:53:25 +0400

    correct icons for saved and likes section

--- src/components/Profile/ProfileTabs.jsx
+++ src/components/Profile/ProfileTabs.jsx
@@ -1,5 +1,5 @@
 import { Box, Flex, Text } from "@chakra-ui/react";
-import {BsGrid3X3} from 'react-icons/bs';
+import {BsBookmark, BsGrid3X3, BsSuitHeart} from 'react-icons/bs';
 
 const ProfileTabs = () => {
   return (
@@ -20,7 +20,7 @@ const ProfileTabs = () => {
 
       <Flex alignItems={"center"} p="3" gap={1} cursor={"pointer"}>
         <Box fontSize={20}>
-          <BsGrid3X3 />
+          <BsBookmark/>
         </Box>
         <Text fontSize={12} display={{base:"none", sm:"block"}}>
           saved
@@ -29,7 +29,7 @@ const ProfileTabs = () => {
 
       <Flex alignItems={"center"} p="3" gap={1} cursor={"pointer"}>
         <Box fontSize={20}>
-          <BsGrid3X3 />
+          <BsSuitHeart fontWeight={"bold"} />
         </Box>
         <Text fontSize={12} display={{base:"none", sm:"block"}}>
           Likes
