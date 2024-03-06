e04a57499cbb9d5a1ec0db764c1d2498f2b91749
astarta16 <ngedeshuri2001@gmail.com>
2024-03-06 14:51:01 +0400

    add likes and saved section

--- src/components/Profile/ProfileTabs.jsx
+++ src/components/Profile/ProfileTabs.jsx
@@ -1,4 +1,4 @@
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Flex, Text } from "@chakra-ui/react";
 import {BsGrid3X3} from 'react-icons/bs';
 
 const ProfileTabs = () => {
@@ -9,10 +9,31 @@ const ProfileTabs = () => {
       gap={{ base: 4, sm: 10 }}
       textTransform={"uppercase"}
       fontWeight={"bold"}>
-      <Flex borderTop={"1px solid white"}>
+      <Flex borderTop={"1px solid white"} alignItems={"center"} p="3" gap={1} cursor={"pointer"}>
         <Box fontSize={20}>
           <BsGrid3X3 />
         </Box>
+        <Text fontSize={12} display={{base:"none", sm:"block"}}>
+          Posts
+        </Text>
+      </Flex>
+
+      <Flex alignItems={"center"} p="3" gap={1} cursor={"pointer"}>
+        <Box fontSize={20}>
+          <BsGrid3X3 />
+        </Box>
+        <Text fontSize={12} display={{base:"none", sm:"block"}}>
+          saved
+        </Text>
+      </Flex>
+
+      <Flex alignItems={"center"} p="3" gap={1} cursor={"pointer"}>
+        <Box fontSize={20}>
+          <BsGrid3X3 />
+        </Box>
+        <Text fontSize={12} display={{base:"none", sm:"block"}}>
+          Likes
+        </Text>
       </Flex>
     </Flex>
   );
