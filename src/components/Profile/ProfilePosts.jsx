f42a639cc1c527e18d5ffdb68bc3f4bcc8845fbb
astarta16 <ngedeshuri2001@gmail.com>
2024-03-11 17:05:30 +0400

    start profile posts section

--- src/components/Profile/ProfilePosts.jsx
+++ src/components/Profile/ProfilePosts.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import { Grid } from "@chakra-ui/react"
+import { useEffect, useState } from "react"
 
-function ProfilePosts() {
-  return (
-    <div>
-      profile
-    </div>
-  )
+const  ProfilePosts = () => {
+  const [isloading, setIsLoading] = useState(true)
+
+  useEffect(() =>{}, [] )
+
+  return 
+    <Grid
+    templateColumns={{sm:"repeat(1, 1fr)",md:"repeat(3, 1fr)"}}
+    gap={1}
+    columnGap={1}
+    
+    >
+
+
+    </Grid>
+
 }
 
 export default ProfilePosts
