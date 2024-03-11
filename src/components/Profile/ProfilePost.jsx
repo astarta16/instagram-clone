c4ec364c2eb35fb3501caf62af3a67f505c8f55b
astarta16 <ngedeshuri2001@gmail.com>
2024-03-11 17:50:02 +0400

    add flex styles

--- src/components/Profile/ProfilePost.jsx
+++ src/components/Profile/ProfilePost.jsx
@@ -1,18 +1,32 @@
-import { GridItem } from "@chakra-ui/react"
+import { Flex, GridItem } from "@chakra-ui/react";
 
- const ProfilePost = () => {
-  return <GridItem
-  cursor={"pointer"}
-  borderRadius={4}
-  overflow={"hidden"}
-  border={"1px solid"}
-  borderColor={"white"}
-  position={"relative"}
-  aspectRatio={1/1}
-  
-  >
+const ProfilePost = () => {
+  return (
+    <GridItem
+      cursor={"pointer"}
+      borderRadius={4}
+      overflow={"hidden"}
+      border={"1px solid"}
+      borderColor={"white"}
+      position={"relative"}
+      aspectRatio={1 / 1}>
 
-  </GridItem>
+        <Flex
+        opacity={0}
+        _hover={{opacity:1}}
+        position={"absolute"}
+        top={0}
+        left={0}
+        right={0}
+        bottom={0}
+        bg={"blackAlpha.700"}
+        zIndex={1}
+        justifyContent={"center"}
+        >
+
+        </Flex>
+      </GridItem>
+  );
 };
 
-export default ProfilePost
+export default ProfilePost;
