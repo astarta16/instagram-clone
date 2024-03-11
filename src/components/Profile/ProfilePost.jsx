01db6c9fc886e00b4334776b797a731ad878cba4
astarta16 <ngedeshuri2001@gmail.com>
2024-03-11 17:46:31 +0400

    start styles for profilepost

--- src/components/Profile/ProfilePost.jsx
+++ src/components/Profile/ProfilePost.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { GridItem } from "@chakra-ui/react"
 
-export default function ProfilePost() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+ const ProfilePost = () => {
+  return <GridItem
+  cursor={"pointer"}
+  borderRadius={4}
+  overflow={"hidden"}
+  border={"1px solid"}
+  borderColor={"white"}
+  position={"relative"}
+
+  
+  >
+
+  </GridItem>
+};
+
+export default ProfilePost
