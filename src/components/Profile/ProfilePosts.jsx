85061fdfaabe0e288633f648fe774a81a8509a25
astarta16 <ngedeshuri2001@gmail.com>
2024-03-11 17:32:18 +0400

    correct syntax error

--- src/components/Profile/ProfilePosts.jsx
+++ src/components/Profile/ProfilePosts.jsx
@@ -1,22 +1,33 @@
-import { Grid } from "@chakra-ui/react"
-import { useEffect, useState } from "react"
+import { Box, Grid, Skeleton, VStack } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 
-const  ProfilePosts = () => {
-  const [isloading, setIsLoading] = useState(true)
+const ProfilePosts = () => {
+  const [isloading, setIsLoading] = useState(true);
 
-  useEffect(() =>{}, [] )
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, []);
 
-  return 
+  return (
     <Grid
-    templateColumns={{sm:"repeat(1, 1fr)",md:"repeat(3, 1fr)"}}
-    gap={1}
-    columnGap={1}
-    
-    >
-
-
+      templateColumns={{
+        sm: "repeat(1, 1fr)",
+        md: "repeat(3, 1fr)",
+      }}
+      gap={1}
+      columnGap={1}>
+      {isloading &&
+        [0, 1, 2, 3, 4, 5].map((_, idx) => (
+          <VStack key={idx} alignItems={"flex-start"} gap={4}>
+            <Skeleton w={"full"}>
+              <Box h="300px"> contents wrapped</Box>
+            </Skeleton>
+          </VStack>
+        ))}
     </Grid>
+  );
+};
 
-}
-
-export default ProfilePosts
+export default ProfilePosts;
