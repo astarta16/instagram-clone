57a10f22d33a3d5024e066726e227489673d872e
astarta16 <ngedeshuri2001@gmail.com>
2024-03-28 19:09:39 +0400

    add modal window, close and open

--- src/components/Profile/ProfilePost.jsx
+++ src/components/Profile/ProfilePost.jsx
@@ -1,57 +1,78 @@
-import { Flex, GridItem, Text, Image, useDisclosure } from "@chakra-ui/react";
+import {
+  Flex,
+  GridItem,
+  Text,
+  Image,
+  useDisclosure,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+} from "@chakra-ui/react";
 import { AiFillHeart } from "react-icons/ai";
 import { FaComment } from "react-icons/fa";
 
 const ProfilePost = ({ img }) => {
-  const {isOpen, onOpen, onClose } = useDisclosure()
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
-    <GridItem
-      cursor={"pointer"}
-      borderRadius={4}
-      overflow={"hidden"}
-      border={"1px solid"}
-      borderColor={"white"}
-      position={"relative"}
-      aspectRatio={1 / 1}
-      onClick={onOpen}
-      >
-      
-      <Flex
-        opacity={0}
-        _hover={{ opacity: 1 }}
-        position={"absolute"}
-        top={0}
-        left={0}
-        right={0}
-        bottom={0}
-        bg={"blackAlpha.700"}
-        zIndex={1}
-        justifyContent={"center"}>
-        <Flex alignItems={"center"} justifyContent={"center"} gap={50}>
-          <Flex>
-            <AiFillHeart size={20} />
-            <Text fontWeight={"bold"} ml={2}>
-              7
-            </Text>
-          </Flex>
-          <Flex>
-            <FaComment size={20} />
-            <Text fontWeight={"bold"} ml={2}>
-              7
-            </Text>
+    <>
+      <GridItem
+        cursor={"pointer"}
+        borderRadius={4}
+        overflow={"hidden"}
+        border={"1px solid"}
+        borderColor={"white"}
+        position={"relative"}
+        aspectRatio={1 / 1}
+        onClick={onOpen}>
+        <Flex
+          opacity={0}
+          _hover={{ opacity: 1 }}
+          position={"absolute"}
+          top={0}
+          left={0}
+          right={0}
+          bottom={0}
+          bg={"blackAlpha.700"}
+          zIndex={1}
+          justifyContent={"center"}>
+          <Flex alignItems={"center"} justifyContent={"center"} gap={50}>
+            <Flex>
+              <AiFillHeart size={20} />
+              <Text fontWeight={"bold"} ml={2}>
+                7
+              </Text>
+            </Flex>
+            <Flex>
+              <FaComment size={20} />
+              <Text fontWeight={"bold"} ml={2}>
+                7
+              </Text>
+            </Flex>
           </Flex>
         </Flex>
-      </Flex>
 
-      <Image
-        src={img}
-        alt="profile post"
-        w={"100%"}
-        h={"100%"}
-        objectFit={"cover"}
-      />
-    </GridItem>
+        <Image
+          src={img}
+          alt="profile post"
+          w={"100%"}
+          h={"100%"}
+          objectFit={"cover"}
+        />
+      </GridItem>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal header</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody></ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
   );
 };
 
