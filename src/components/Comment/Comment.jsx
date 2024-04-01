d5583e03f9c80c8628a3e479c67082caa83ebe40
astarta16 <ngedeshuri2001@gmail.com>
2024-04-01 10:05:37 +0400

    start working in comments styles

--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -1,7 +1,17 @@
+import { Avatar, Flex, Text } from "@chakra-ui/react";
 import React from "react";
 
 const Comment = ({ createAt, username, profilePic, text }) => {
-  return <div>comment</div>;
+  return <Flex gap={4}>
+    <Avatar src={profilePic}  name={username} size={"sm"}/>
+    <Flex direction={"column"}>
+      <Flex gap={2}>
+        <Text fontWeight={"bold"}>
+          {username}
+        </Text>
+      </Flex>
+    </Flex>
+     </Flex>
 };
 
 export default Comment;
